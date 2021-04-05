88907e76273fd58ed1125ce3dfe2dd875995cacd
Tanvi <tanvi.pandit@hcl.com>
2021-04-05 09:31:23 -0400

    Deleting older commits

--- src/SynGossipDigest.py
+++
@@ -1,8 +0,0 @@
-class SynGossipDigest:
-
-    def __init__(self, clusterId, gDigestList):
-        self._clusterId = clusterId
-        self._gDigestList = gDigestList
-
-    def getGossipDigest(self):
-        return self._gDigestList
