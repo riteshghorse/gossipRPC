12e4f4adb2634dedf11b075a2b70437ef3933282
riteshghorse <riteshghorse@gmail.com>
2021-04-19 20:09:24 -0400

    sequence check done

--- Monitoring_Node.py
+++ Monitoring_Node.py
@@ -58,12 +58,19 @@ class MonitoringNode:
 
 
     def updateSuspectMatrix(self, ip, fault_vector, generation):
-        
-        index = self.getMapping()[ip]
+        print(ip, fault_vector)
+        try:
+            index = self.getMapping()[ip]
+        except Exception as e:
+            pass
+
         for k,v in fault_vector.items():
             if v==0:
-                if(self.IP_to_Node_Index[k] in self.ip_generation and generation == self.ip_generation[self.IP_to_Node_Index[k]]):
-                    print('False failure detection happened for ', k)        
+                try:
+                    if(self.IP_to_Node_Index[k] in self.ip_generation and generation == self.ip_generation[self.IP_to_Node_Index[k]]):
+                        print('False failure detection happened for ', k)        
+                except Exception as e:
+                    pass
             # print(self.IP_to_Node_Index, k)
             # if v==0 and (self.IP_to_Node_Index[k] in self.global_fault_vector) and self.global_fault_vector[self.IP_to_Node_Index[k]] == 1:
             #     print(self.IP_to_Node_Index, k)
@@ -102,22 +109,24 @@ class MonitoringNode:
             for i in range(len(self.suspect_matrix)):
                 if i != j and ((self.global_fault_vector[i]) != 1):
                     # print('first if')
-                    if getDiffInSeconds(self.heartbeatTime[self.Index_to_IP[i]]) < Constants.WAIT_SECONDS_FAIL:
-                        # print('second if')
-                        state &= self.suspect_matrix[i][j]
+                    try:
+                        if getDiffInSeconds(self.heartbeatTime[self.Index_to_IP[i]]) < Constants.WAIT_SECONDS_FAIL:
+                            state &= self.suspect_matrix[i][j]
+                    except Exception as e:
+                        pass
             if(state == 1):
                 # print(j, self.Index_to_IP[j])
-                # print(self.suspect_matrix)
+                print(self.suspect_matrix)
                 print("Node %s is failed" % (self.Index_to_IP[j]))                
             else:
                 print("Node %s is alive" % (self.Index_to_IP[j]))                
             self.global_fault_vector[j] = state
             if(state == 1):
                 # print("global state map----------------//////////\n", len(self.global_state_map))
-                # try:
-                self.ip_generation[j] = self.global_state_map[self.Index_to_IP[j]][self.Index_to_IP[j]]['heartBeat']['generation']
-                # except Exception as e:
-                #     pass
+                try:
+                    self.ip_generation[j] = self.global_state_map[self.Index_to_IP[j]][self.Index_to_IP[j]]['heartBeat']['generation']
+                except Exception as e:
+                    pass
 
                 print('in do consensus generation', self.ip_generation)
         print('end consensus')
